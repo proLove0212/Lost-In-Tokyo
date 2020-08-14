2c73d61b1c9e540cfceb1c29ad8e949db87b082a
PROLOVE <proLove0212@gmail.com>
2020-08-14 17:09:53 -0400

    Creating attractions component

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
-const Highlight = props => (
-  <span className={`relative highlight highlight-${props.color}`}>
-    <span className="relative z-2">{props.children}</span>
+const Highlight = ({ color, children }) => (
+  <span className={`relative highlight highlight-${color}`}>
+    <span className="relative z-2">{children}</span>
   </span>
 );
 
 const Intro = () => (
   <div className="m-auto-ns f4 f3-m f2-1 tc w-80-1 normal">
     <div className="mb3 mb4-ns">
-      <Highlight color='aqua'>Lost in Tokyo</Highlight> is a directory of fun places to see, play in and <Highlight color='yellow'>explore</Highlight>, in <Highlight color='blue'>Tokyo</Highlight>, Japan.
+      <Highlight color='aqua'>Lost in Tokyo</Highlight> is a directory of fun places to see, play in, and <Highlight color='yellow'>explore</Highlight> in <Highlight color='blue'>Tokyo</Highlight>, Japan.
   </div>
     <div>
-      From <Highlight color='blue'>museums</Highlight> and <Highlight color='blue'>galleries</Highlight>, to <Highlight color='pink'>Robot Restaurants</Highlight> and <Highlight color='pink'>kitten cafes</Highlight>, Tokyo is the gift that keeps on giving. <Highlight color='yellow'>Dattebayo!</Highlight>
+      From <Highlight color='blue'>museums</Highlight> and <Highlight color='blue'>galleries</Highlight> to <Highlight color='pink'>Robot Restaurants</Highlight> and <Highlight color='pink'>kitten cafes</Highlight>, Tokyo is the gift that keeps on giving. <Highlight color='yellow'>Dattebayo!</Highlight>
     </div>
   </div>
 );
 
-const NavItem = props => (
-  <li className={`mh2-ns f6 f4-1 tc ${item.className}`}>
-    <a className='white no-underline' href={item.href}>{item.children}</a>
+const NavItem = ({ className, href, children, logo }) => (
+  <li className={`mh2-ns f6 f4-1 tc ${className}`}>
+    <a className='white no-underline' href={href}>
+      {logo ? <img src="../images/logo.svg" className="db center logo" /> : children}
+    </a>
   </li>
 )
 
@@ -28,6 +30,14 @@ const Nav = () => (
         <NavItem {...item} />)}
     </ul>
   </nav>
+);
+
+const Attraction = ({ title, description, image, className }) => (
+  <div className={className}>
+    <h1>{title}</h1>
+    <p>{description}</p>
+    <img src={`../images/${image}`} />
+  </div>
 );
 
 const App = () => (
