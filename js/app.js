13175f82857d858eb2bd82443c481a319f11d5ec
PROLOVE <proLove0212@gmail.com>
2020-08-14 13:43:34 -0400

    Combining components

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-const Hello = props => <h1>Hello {props.name}</h1>;
+const Title = props => (
+  <h1 className="tc">{props.title}</h1>
+);
 
-ReactDOM.render(<Hello name="Lawrence" />, document.getElementById('root'));
+const Banner = props => (
+  <h1 className="tc f1 yellow pa3">
+    Hello {props.firstName} {props.lastName}
+  </h1>
+);
+
+const App = () => (
+  <div>
+    <Title title="Welcome to my website!" />
+    <Banner firstName="Madeline" lastName="Uribes" />
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
